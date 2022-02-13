8ba0cbf9701839ed06605e9bf01f3de35752e44f
elye <elye.project@gmail.com>
2022-02-13 21:16:01 +1100

    resize with dynamic text

--- app/src/main/java/com/example/composeanimation/AnimateContentSize.kt
+++ app/src/main/java/com/example/composeanimation/AnimateContentSize.kt
@@ -1,34 +1,62 @@
 package com.example.composeanimation
 
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.animateContentSize
+import android.util.Log
+import androidx.compose.animation.*
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun AnimateContentSize() {
+
     val letter = 'X'
     var message by remember { mutableStateOf(letter.toString()) }
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Box(
             modifier = Modifier
                 .background(Color.Green)
-                .animateContentSize()
+                .animateContentSize(
+                    animationSpec = spring(0.5f, 500f),
+                    finishedListener = { _, _ ->
+                        message = newText(letter)
+                    }
+                )
         ) {
             Text(text = message)
         }
 
         Button(onClick = {
-            message = letter.toString().padEnd((0..30).random(), letter)
+            message = newText(letter)
         }) {
             Text("Click Me")
         }
     }
+}
+
+private fun newText(letter: Char): String {
+    val count = (0..30).random()
+    var newText = ""
+    repeat(count) {
+        if (newText != "") newText += "\n"
+        newText += letter
+            .toString()
+            .padEnd(count, letter)
+    }
+    return newText
 }
