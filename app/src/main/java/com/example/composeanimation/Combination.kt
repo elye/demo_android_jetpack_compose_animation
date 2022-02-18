3425c98939cb8e18d40ce165d1465562dd944f3a
elye <elye.project@gmail.com>
2022-02-18 21:18:21 +1100

    Add both spring and bounce and using offset

--- app/src/main/java/com/example/composeanimation/Combination.kt
+++ app/src/main/java/com/example/composeanimation/Combination.kt
@@ -1,6 +1,6 @@
 package com.example.composeanimation
 
-import androidx.compose.animation.animateColor
+import android.view.animation.BounceInterpolator
 import androidx.compose.animation.core.*
 import androidx.compose.animation.core.Animatable
 import androidx.compose.foundation.background
@@ -11,8 +11,10 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.example.composeanimation.ui.theme.ComposeAnimationTheme
 
 @Composable
 fun Combination() {
@@ -20,14 +22,21 @@ fun Combination() {
 
     val dbAnimateAsState: Dp by animateDpAsState(
         targetValue = switch(enabled),
-        animationSpec = animationSpec()
+        animationSpec = if (enabled) animationSpec() else bounceAnimationSpec()
     )
 
     val dbAnimatable = remember { Animatable(0.dp) }
 
     val transition = updateTransition(enabled, label = "")
     val dbTransition by transition.animateDp(
-        transitionSpec = { animationSpec() }, label = "") {
+        transitionSpec = {
+            if (targetState) {
+                animationSpec()
+            } else {
+                bounceAnimationSpec()
+            }
+        }, label = ""
+    ) {
         switch(it)
     }
 
@@ -52,16 +61,18 @@ fun Combination() {
     LaunchedEffect(key1 = enabled) {
         dbAnimatable.animateTo(
             targetValue = switch(enabled),
-            animationSpec = animationSpec()
+            animationSpec = if (enabled) animationSpec() else bounceAnimationSpec()
         )
     }
 }
 
+private fun animationSpec(): SpringSpec<Dp> =
+    spring(stiffness =20f, dampingRatio = 0.25f)
 
-private fun animationSpec(): TweenSpec<Dp> =
+private fun bounceAnimationSpec(): TweenSpec<Dp> =
     tween(
         durationMillis = 3000,
-        easing = LinearOutSlowInEasing
+        easing = BounceInterpolator().toEasing()
     )
 
 private fun switch(enabled: Boolean) = if (enabled) 268.dp else 0.dp
@@ -84,10 +95,18 @@ private fun animateBoxHorizontal(dbAnimateAsState: Dp) {
     ) {
         Box(
             modifier = Modifier
-                .padding(dbAnimateAsState, 0.dp, 0.dp, 0.dp)
+                .offset(dbAnimateAsState, 0.dp)
                 .size(32.dp)
                 .background(Color.Red)
         )
     }
     Spacer(modifier = Modifier.height(16.dp))
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultedPreview() {
+    ComposeAnimationTheme {
+        Combination()
+    }
 }
